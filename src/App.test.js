61ada2f4d26f9189556808233ca502328e363ac6
nandagopal <nandagopal@users.noreply.github.com>
2020-08-10 09:24:56 +0530

    test changed

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("dummy test", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/NandaGopal/);
   expect(linkElement).toBeInTheDocument();
