6a05980b4a0a7bdbfce17d9bd822b2a976be9fd3
ChanManChan <ngopal253@gmail.com>
2020-08-11 12:16:47 +0530

    fix(typo): type fix

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3444,31 +3444,29 @@ exports.getState = getState;
 /***/ 482:
 /***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
 
-const core = __webpack_require__(470)
-const github = __webpack_require__(469)
-
+const core = __webpack_require__(470);
+const github = __webpack_require__(469);
 
 try {
   // we called it "token" in action.yml
-  const token = core.getInput('token')
-  const title = core.getInput('title')
-  const body = core.getInput('body')
-  const assignees = core.getInput('assignees')
+  const token = core.getInput("token");
+  const title = core.getInput("title");
+  const body = core.getInput("body");
+  const assignees = core.getInput("assignees");
 
-  const octokit = new github.Github(token)
+  const octokit = new github.GitHub(token);
   const response = octokit.issues.create({
     // owner:github.context.repo.owner,
     // repo:github.context.repo.repo,
     ...github.context.repo,
     title,
     body,
-    assignees: assignees ? assignees.split('\n') : undefined
-  })
+    assignees: assignees ? assignees.split("\n") : undefined
+  });
 
-  core.setOutput('issue', JSON.stringify(response.data))
-
+  core.setOutput("issue", JSON.stringify(response.data));
 } catch (error) {
-  core.setFailed(error.message)
+  core.setFailed(error.message);
 }
 
 
