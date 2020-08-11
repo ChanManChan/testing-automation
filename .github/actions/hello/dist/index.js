d395bd9e006df7f1f5e37e7b0e3affe5f1394f3a
ChanManChan <ngopal253@gmail.com>
2020-08-11 10:48:24 +0530

    feat(log messages): various log messages

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -681,14 +681,29 @@ const github = __webpack_require__(469);
 
 try {
   // simulate an error by throwing an error
-  throw new Error("testing action and workflow fail state");
+  // throw new Error("testing action and workflow fail state");
+  // as action author, we need to provide "ACTIONS_STEP_DEBUG" logs to people who are using our action
+  // this debug message will only appear when debugging is enabled
+  core.debug('Debug message')
+  core.warning('Warning message')
+  core.error('Error message')
+
+  // this will not be logged as 'Nandu' but as ***
+  core.setSecret('Nandu')
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name)
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  // startGroup <- expandable
+  core.startGroup('Logging github object')
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup()
+  // env variables that we can use in any subsequent steps
+  core.exportVariable('HELLO', 'hello')
 } catch (e) {
   // to make github aware that our action has failed call
   core.setFailed(e.message);
