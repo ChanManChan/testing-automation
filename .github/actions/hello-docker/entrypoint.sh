634c324376554c9f1f7e61dded0187830f35bb42
ChanManChan <ngopal253@gmail.com>
2020-08-13 18:47:22 +0530

    feat(actions): docker greet action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -l
+
+# Equivalent logic in "actions/hello/index.js"
+#? core.debug("Debug message");
+#? core.warning("Warning message");
+#? core.error("Error message");
+
+echo "::debug ::Debug Message"
+echo "::warning ::Warning Message"
+echo "::error ::Error Message"
+
+#? core.setSecret("Nandu");
+
+echo "::add-mask::$1"
+#? we need this argument to be our input
+echo "Hello $1"
+
+
+#? const time = new Date();
+#? core.setOutput("time", time.toTimeString());
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+#? core.startGroup("Logging github object");
+#? console.log(JSON.stringify(github, null, "\t"));
+#? core.endGroup();
+
+
+echo "::group::Some expandable logs"
+echo "some stuff"
+echo "some stuff"
+echo "some stuff"
+echo "some stuff"
+echo "::endgroup::"
+
+#? core.exportVariable("HELLO", "hello");
+
+echo "::set-env name=HELLO::hello"
